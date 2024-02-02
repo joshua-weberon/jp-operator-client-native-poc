98c571ca9c75fb36e516d040211ec898b680fd6b
joshua-weberon <joshua@weberon.net>
2024-02-02 13:49:46 +0530

    code clean up in App.js

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from "react-redux";
 
 import {store} from './state/store';
 import ScrollChat from "./components/Scroll/index";
-import PaneViews from "./components/PaneViews/index"
+//import PaneViews from "./components/PaneViews/index"
 
 
 export default function App() {
-  /* return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  ); */
+  
   /* return (
     <PaneViews />
   ) */
